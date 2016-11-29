09aeb5139968c5ec910981b2748a945787163337
Ebenezer Abu Ohene-Adutwum <ebenezer.ohene-adutwum@st.vvu.edu.gh>
2016-11-29 16:58:19 +0000

    Add files via upload

---
+++ NexusTechBot/htm/News.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+    <link href="../css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/main.css" rel="stylesheet" type="text/css">
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    
+
+</head>
+<body>
+    
+    <nav class="nav navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <img id="m_logo" width="200px" src="../img/log0.png" >       
+                <div class="navbar-collapse collapse">
+                    <ul id="menu_bar" class="nav navbar-nav navbar-right">
+                        <li><a href="../index.php">Home</a></li>
+                        <li class="active"><a href="../htm/News.php">News</a></li>
+                        <li><a href="../htm/about_us.php">About Us</a></li>
+                    </ul>
+                </div>  
+            </div>
+    </nav>
+    
+    
+<!--jQuery (necessary for Bootstrap's JavaScript plugins))-->
+            <script src="https://ajax.googleapi.com/ajas/libs/jquery/1.11.3/jquery.min.js"></script>
+            <!--Include all compiled plugins (below), or include individual files as needed-->
+            <script src="../js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ NexusTechBot/js/index.js
@@ -0,0 +1,21 @@
+ $("#login-button").click(function(event){
+		 event.preventDefault();
+	 
+	 $('form').fadeOut(500);
+	 $('.wrapper').addClass('form-success');
+});
+
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
